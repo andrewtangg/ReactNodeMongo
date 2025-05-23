71db34189870a37f945ea726158bbc6cbe7284b5
Andrew Tang <andrewtangg@yahoo.com>
2025-05-23 16:59:18 -0500

    Added Navbar and Layout components, and moved articles list to its own page.

---
+++ front-end/src/pages/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return(
+        <>
+            <h1>Page Not Found</h1>
+            <p>The link you have must have been broken</p>
+        </>
+    )
+}
