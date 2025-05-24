655e5c0c4b3f28b328cd62d8464c17be0b78ed93
Andrew Tang <andrewtangg@yahoo.com>
2025-05-24 18:12:35 -0500

    Added post request to extract comments from request body and saving it a array

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -4,9 +4,9 @@
 //because we defined npx nodemon src/server.js in the scripts for dev, we can just use npm run dev
 // This is a simple Express server that listens on port 8000
 const articleInfo = [
-    {name: 'learn-node', upvotes: 0},
-    {name: 'learn-react', upvotes: 0},
-    {name: 'mongodb', upvotes: 0}
+    {name: 'learn-node', upvotes: 0, comments: []},
+    {name: 'learn-react', upvotes: 0, comments: []},
+    {name: 'mongodb', upvotes: 0, comments: []}
 ]
 
 import express from 'express';
@@ -19,9 +19,24 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const article = articleInfo.find(a => a.name === req.params.name);
     article.upvotes += 1;
 
-    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!');
+    res.json(article);
 });
 
+app.post('/api/articles/:name/comments', (req, res) => {
+    // Extracting the article name from the request parameters
+    const { name } = req.params;
+    //destructuring the request body to get postedBy and text
+    const {postedBy, text} = req.body;
+
+    //find the article with the name
+    const article = articleInfo.find(a => a.name === name);
+
+    //pushes the requested comment to the article's comments array
+    article.comments.push({postedBy,text});
+
+    //used to send back json response
+    res.json(article)
+});
 //listening on port 8000
 app.listen(8000, function() {
     console.log('Server is listening on port 8000');
