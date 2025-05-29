5bb9cdff61bb28529217ccdeb9d47336b5b988ee
Andrew Tang <andrewtangg@yahoo.com>
2025-05-29 00:03:06 -0500

    Added MongoDB connection string URL and BSON libraries to the back-end.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,43 +1,81 @@
-
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 //After npm install --save-dev nodemon, you can run the server with nodemon by using the command:
 //npx nodemon src/server.js
 //because we defined npx nodemon src/server.js in the scripts for dev, we can just use npm run dev
 // This is a simple Express server that listens on port 8000
-const articleInfo = [
-    {name: 'learn-node', upvotes: 0, comments: []},
-    {name: 'learn-react', upvotes: 0, comments: []},
-    {name: 'mongodb', upvotes: 0, comments: []}
-]
 
-import express from 'express';
 const app = express();
 
 app.use(express.json()); // If request sees a json request, process it in req.body.name
 
-// This post request will return message with upvoted counts from in memory
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const article = articleInfo.find(a => a.name === req.params.name);
-    article.upvotes += 1;
+let db;
+
+async function connrectToDb() {
+    const uri = 'mongodb://127.0.0.1:27017';
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+
+    db = client.db('full-stack-react-db');
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+
+
+    const article = await db.collection('articles').findOne({ name });
 
     res.json(article);
+
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+
+// This post request will return message with upvoted counts from in memory
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params
+    const updatedArticle = await db.collection('articles').findOneAndUpdate(
+        { name }, 
+        {$inc: {upvotes: 1 }
+    });
+
+    res.json(updatedArticle);
+});
+
+app.post('/api/articles/:name/comments', async (req, res) => {
     // Extracting the article name from the request parameters
     const { name } = req.params;
     //destructuring the request body to get postedBy and text
     const {postedBy, text} = req.body;
+    //Grabs the postedBy and text from the request body
+    const newComment = { postedBy, text};
 
-    //find the article with the name
-    const article = articleInfo.find(a => a.name === name);
+    //Inserts the new comment into the comments array of the article with the specified name
+    const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, 
+            {$push: { comments: newComment}},
+            {
+                // This option ensures that the updated document is returned after the update operation
+                returnDocument: 'after',
+            }
+    );
 
-    //pushes the requested comment to the article's comments array
-    article.comments.push({postedBy,text});
-
-    //used to send back json response
-    res.json(article)
+    res.json(updatedArticle);
 });
-//listening on port 8000
-app.listen(8000, function() {
+
+async function start() {
+        await connrectToDb();
+        //listening on port 8000
+    app.listen(8000, function() {
     console.log('Server is listening on port 8000');
 });
+    }
+
+start();
